cd007f3ba24a14c3717caf22185983f255d9a719
ayaan <ayaan@t-win.kr>
2023-10-19 15:06:35 +0900

    implement theme-provider

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,3 +1,11 @@
+import BottomNavigation from '@/components/bottom-navigation';
+import { Outlet } from 'react-router-dom';
+
 export default function Layout() {
-  return <h1>Layout !</h1>;
+  return (
+    <main className="h-screen grid grid-rows-[90%_10%]">
+      <Outlet />
+      <BottomNavigation />
+    </main>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import router from '@/router';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import ThemeProvider from '@/components/theme-provider';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 );
