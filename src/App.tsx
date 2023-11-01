1a9584d1b0a4c5db3afd7e72459da0448dcd6e81
ayaan <ayaan@t-win.kr>
2023-11-01 15:46:28 +0900

    use react-query

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import Loading from '@/components/loading';
 import ThemeProvider from '@/components/theme-provider';
 import { auth } from '@/config/firebase';
 import router from '@/router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -17,14 +20,16 @@ export default function App() {
   }, []);
 
   return (
-    <ThemeProvider>
-      {isLoading ? (
-        <main className="flex h-screen items-center justify-center bg-light-background-main text-light-text-main dark:bg-dark-background-main dark:text-dark-text-main">
-          <Loading />
-        </main>
-      ) : (
-        <RouterProvider router={router} />
-      )}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        {isLoading ? (
+          <main className="flex h-screen items-center justify-center bg-light-background-main text-light-text-main dark:bg-dark-background-main dark:text-dark-text-main">
+            <Loading />
+          </main>
+        ) : (
+          <RouterProvider router={router} />
+        )}
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
