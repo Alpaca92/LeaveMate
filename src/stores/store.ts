6a82f7afc971fe49968e3dfc4819ebc11465b5f2
ayaan <ayaan@t-win.kr>
2023-11-07 17:38:58 +0900

    (testing) zustand devtools

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,10 +1,9 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 import { membersSlice } from '@/stores/slices/membersSlice';
+import { currentUserSlice } from '@/stores/slices/currentUserSlice';
 import type { MembersSlice } from '@/stores/slices/membersSlice';
-import {
-  CurrentUserSlice,
-  currentUserSlice,
-} from '@/stores/slices/currentUserSlice';
+import type { CurrentUserSlice } from '@/stores/slices/currentUserSlice';
 
 const RootStore = create<MembersSlice & CurrentUserSlice>()((...a) => ({
   ...membersSlice(...a),
