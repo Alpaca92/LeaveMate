bf8b5151ac3e0425090c42f1c802a42dc72310aa
ayaan <ayaan@t-win.kr>
2023-11-08 12:52:14 +0900

    change args name

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,13 +1,12 @@
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
 import { membersSlice } from '@/stores/slices/membersSlice';
 import { currentUserSlice } from '@/stores/slices/currentUserSlice';
 import type { MembersSlice } from '@/stores/slices/membersSlice';
 import type { CurrentUserSlice } from '@/stores/slices/currentUserSlice';
 
-const RootStore = create<MembersSlice & CurrentUserSlice>()((...a) => ({
-  ...membersSlice(...a),
-  ...currentUserSlice(...a),
+const RootStore = create<MembersSlice & CurrentUserSlice>()((...args) => ({
+  ...membersSlice(...args),
+  ...currentUserSlice(...args),
 }));
 
 export default RootStore;
