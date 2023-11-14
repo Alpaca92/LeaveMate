ce7796565e7a1e7122a855dbbdde1bb17695d895
ayaan <ayaan@t-win.kr>
2023-11-14 17:14:45 +0900

    Add kind prop to support future input types

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -2,23 +2,28 @@ import { UseFormRegisterReturn } from 'react-hook-form';
 
 interface InputProps {
   type?: 'email' | 'text' | 'password';
+  kind?: 'input'; // FIXME: 추후에 input의 역할이 다양해질 것을 대비
   placeholder: string;
   register: UseFormRegisterReturn;
 }
 
 export default function Input({
   type = 'text',
+  kind = 'input',
   placeholder,
   register,
   ...rest
 }: InputProps) {
-  return (
-    <input
-      className="rounded-lg px-3 py-2 focus:outline-none"
-      type={type}
-      placeholder={placeholder}
-      {...register}
-      {...rest}
-    />
-  );
+  switch (kind) {
+    case 'input':
+      return (
+        <input
+          className="rounded-lg px-3 py-2 focus:outline-none"
+          type={type}
+          placeholder={placeholder}
+          {...register}
+          {...rest}
+        />
+      );
+  }
 }
