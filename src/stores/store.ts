00420a03b1051e12c71ce12621c6fd422e560757
ayaan <ayaan@t-win.kr>
2023-11-14 14:17:27 +0900

    add devtools on zustand store

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -3,10 +3,13 @@ import { membersSlice } from '@/stores/slices/membersSlice';
 import { currentUserSlice } from '@/stores/slices/currentUserSlice';
 import type { MembersSlice } from '@/stores/slices/membersSlice';
 import type { CurrentUserSlice } from '@/stores/slices/currentUserSlice';
+import { devtools } from 'zustand/middleware';
 
-const RootStore = create<MembersSlice & CurrentUserSlice>()((...args) => ({
-  ...membersSlice(...args),
-  ...currentUserSlice(...args),
-}));
+const RootStore = create<MembersSlice & CurrentUserSlice>()(
+  devtools((...args) => ({
+    ...membersSlice(...args),
+    ...currentUserSlice(...args),
+  })),
+);
 
 export default RootStore;
