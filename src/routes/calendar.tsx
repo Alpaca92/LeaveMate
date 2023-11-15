1a88d23c518a1b3b43844e6b98a0cecc646aa234
ayaan <ayaan@t-win.kr>
2023-11-15 17:33:04 +0900

    install react-big-calendar

--- src/routes/calendar.tsx
+++ src/routes/calendar.tsx
@@ -1,3 +1,22 @@
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
+
+import moment from 'moment';
+
+const localizer = momentLocalizer(moment);
+
 export default function Calendar() {
-  return <h1>Calendar !</h1>;
+  return (
+    <article className="flex items-center justify-center px-[10%]">
+      <BigCalendar
+        localizer={localizer}
+        startAccessor="start"
+        endAccessor="end"
+        className="!h-[80%]"
+      />
+    </article>
+  );
 }
+
+// TODO: https://velog.io/@yunju/달력-상태관리하기
