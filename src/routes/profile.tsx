77b903529e2fba02b798e5f374d51b9426c191de
ayaan <ayaan@t-win.kr>
2023-11-17 16:44:27 +0900

    Use mutation to update store values before modifying the profile during 'update profile'

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -19,7 +19,7 @@ import { Theme, ToastContainer, toast } from 'react-toastify';
 import Input from '@/components/input';
 import Button from '@/components/button';
 import ThemeSwitch from '@/components/theme-switch';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 interface ProfileInput {
   name: string;
   email: string;
@@ -66,6 +66,26 @@ export default function Profile() {
     });
   };
 
+  const { mutateAsync } = useMutation({
+    mutationFn: async ({ approver, email, name }: ProfileInput) => {
+      try {
+        await updateDoc(
+          doc(collection(db, COLLECTIONS_NAME.USERS), currentUser.userId),
+          {
+            name,
+            email,
+            approver,
+          },
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: (_, { approver }) => {
+      updateCurrentUser({ approver });
+    },
+  });
+
   const onAvatarUpdate = async (event: React.ChangeEvent<HTMLInputElement>) => {
     if (!user) return;
 
@@ -85,30 +105,12 @@ export default function Profile() {
     }
   };
 
-  const { mutateAsync } = useMutation({
-    mutationFn: async ({ approver, email, name }: ProfileInput) => {
-      try {
-        await updateDoc(
-          doc(collection(db, COLLECTIONS_NAME.USERS), currentUser.userId),
-          {
-            name,
-            email,
-            approver,
-          },
-        );
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  });
-
   const onProfileUpdate = async (data: ProfileInput) => {
     if (!Utils.hasNoEmptyValues(data)) return;
 
     try {
       setIsLoading(true);
       await mutateAsync(data);
-      // updateCurrentUser({ approver }); // FIXME: mutation 되고 난 후 zustand store update 하기
     } catch (error) {
       console.error(error);
 
