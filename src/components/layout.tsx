c0046b713e42cc06262076b7938dfc1a30cd036c
ayaan <ayaan@t-win.kr>
2023-12-15 10:08:20 +0900

    Annotate communication with the server for testing

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,11 +3,9 @@ import Loading from '@/components/loading';
 import { QUERY_KEYS } from '@/config/config';
 import Utils from '@/utils';
 import { useEffect } from 'react';
-import { useQueries, useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { Outlet } from 'react-router-dom';
 import RootStore from '@/stores/store';
-
-const TODAY = new Date();
 
 export default function Layout() {
   const { addHolidays, setCurrentUser, setMembers, setRequests } = RootStore(
@@ -18,82 +16,47 @@ export default function Layout() {
       setRequests,
     }),
   );
+  // const { isSuccess: currentUserSuccess, data: currentUserData } = useQuery({
+  //   queryKey: [QUERY_KEYS.FIRESTORE, QUERY_KEYS.CURRENT_USER],
+  //   queryFn: Utils.fetchCurrentUser,
+  // });
+  // const { isSuccess: membersSuccess, data: membersData } = useQuery({
+  //   queryKey: [QUERY_KEYS.FIRESTORE, QUERY_KEYS.MEMBERS],
+  //   queryFn: Utils.fetchMembers,
+  // });
+  // const { isSuccess: requestsSuccess, data: requestsData } = useQuery({
+  //   queryKey: [QUERY_KEYS.FIRESTORE, QUERY_KEYS.REQUESTS],
+  //   queryFn: Utils.fetchRequests,
+  // });
 
-  const [
-    { data: previousHolidays, isSuccess: previousHolidaysSuccess },
-    { data: currentHolidays, isSuccess: currentHolidaysSuccess },
-    { data: nextHolidays, isSuccess: nextHolidaysSuccess },
-  ] = useQueries({
-    // previous, current, next month
-    queries: new Array(3).fill(0).map((_, i) => ({
-      queryKey: [
-        QUERY_KEYS.HOLIDAYS,
-        TODAY.getFullYear(),
-        TODAY.getMonth() + i,
-      ],
-      queryFn: () =>
-        Utils.fetchPublicHolidays({
-          year: TODAY.getFullYear(),
-          month: TODAY.getMonth() + i,
-        }),
-    })),
-  });
-  const { isSuccess: currentUserSuccess, data: currentUserData } = useQuery({
-    queryKey: [QUERY_KEYS.FIRESTORE, QUERY_KEYS.CURRENT_USER],
-    queryFn: Utils.fetchCurrentUser,
-  });
-  const { isSuccess: membersSuccess, data: membersData } = useQuery({
-    queryKey: [QUERY_KEYS.FIRESTORE, QUERY_KEYS.MEMBERS],
-    queryFn: Utils.fetchMembers,
-  });
-  const { isSuccess: requestsSuccess, data: requestsData } = useQuery({
-    queryKey: [QUERY_KEYS.FIRESTORE, QUERY_KEYS.REQUESTS],
-    queryFn: Utils.fetchRequests,
-  });
+  // useEffect(() => {
+  //   if (
+  //     currentUserSuccess &&
+  //     membersSuccess &&
+  //     requestsSuccess &&
+  //     currentUserData &&
+  //     membersData &&
+  //     requestsData
+  //   ) {
+  //     setCurrentUser(currentUserData);
+  //     setMembers(membersData);
+  //     setRequests(requestsData);
+  //   }
+  // }, [
+  //   addHolidays,
+  //   setCurrentUser,
+  //   setMembers,
+  //   setRequests,
+  //   currentUserSuccess,
+  //   membersSuccess,
+  //   requestsSuccess,
+  //   currentUserData,
+  //   membersData,
+  //   requestsData,
+  // ]);
 
-  useEffect(() => {
-    if (
-      previousHolidaysSuccess &&
-      currentHolidaysSuccess &&
-      nextHolidaysSuccess &&
-      currentUserSuccess &&
-      membersSuccess &&
-      requestsSuccess &&
-      previousHolidays &&
-      currentHolidays &&
-      nextHolidays &&
-      currentUserData &&
-      membersData &&
-      requestsData
-    ) {
-      addHolidays(previousHolidays);
-      addHolidays(currentHolidays);
-      addHolidays(nextHolidays);
-      setCurrentUser(currentUserData);
-      setMembers(membersData);
-      setRequests(requestsData);
-    }
-  }, [
-    addHolidays,
-    setCurrentUser,
-    setMembers,
-    setRequests,
-    previousHolidaysSuccess,
-    currentHolidaysSuccess,
-    nextHolidaysSuccess,
-    currentUserSuccess,
-    membersSuccess,
-    requestsSuccess,
-    previousHolidays,
-    currentHolidays,
-    nextHolidays,
-    currentUserData,
-    membersData,
-    requestsData,
-  ]);
-
-  if (!currentUserSuccess || !membersSuccess || !requestsSuccess)
-    return <Loading />;
+  // if (!currentUserSuccess || !membersSuccess || !requestsSuccess)
+  //   return <Loading />;
 
   return (
     <>
